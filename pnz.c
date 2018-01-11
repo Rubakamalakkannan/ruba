518c1c152381dca49264d184eec56a16f6cf5718
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-01-11 10:33:04 +0000

    Create pnz.c

---
+++ pnz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+{
+int main()
+int num;
+printf("enter the values");
+scanf("%d",&num);
+if(num>0)
+{
+printf("the num is positive");
+}
+if(num<0)
+{
+printf("the num is negative");
+}
+if(num==0)
+{
+printf("the num is zero");
+}
+return 0;
+}
