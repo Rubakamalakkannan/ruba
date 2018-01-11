6e25fc7ebf3cfbe616dff33bac3105db919de5e0
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-01-11 11:00:41 +0000

    Create en or odd.c

---
+++ en or odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the values");
+scanf("%d",&num);
+if(num %2==0)
+{
+printf("the the num is even");
+}
+else
+{
+printf("the num is odd");
+}
+return 0;
+}
