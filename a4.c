380038ef4f4647a4660e61738929b2196811ae38
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-01-19 16:09:59 +0530

    Update a4.c

--- a4.c
+++ a4.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter a character: ");
     scanf("%c",&c);
 
-    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+    if( c == 'a'||c == 'b'||c == 'c'||c =='d'||c == 'e')
         printf("%c is an alphabet.",c);
     else
         printf("%c is not an alphabet.",c);
