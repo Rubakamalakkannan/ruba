0e34acef115d2e1aaaf5619f61ac130c585a4d37
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-01-21 15:44:49 +0530

    Create b4.c

---
+++ b4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,n,count;
+scanf("%d",&n);
+for(a=1,a<=n-1,a++)
+{
+if(n%a == 0)
+c++;
+}
+if(c==2)
+{	
+printf("%d is not a prime number",n);
+}
+else
+{
+printf("%d is a prime nummber",n);
+}
+return 0;
+}
+	
