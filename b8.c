70ef7d9c12c94c49a1ef6f649f71b227052a94c9
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-01-23 11:12:46 +0000

    Create b8.c

---
+++ b8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,n,arm=0,b;
+	scanf("%d",&n);
+
+for(a=n;a<=b;a++)
+	{
+		if(a%2 == 0)
+		{
+
+			printf("%d\n",a);
+		
+
+	}
+while(n!=0)
+{
+a=n%10;
+a=arm+(a*a*a);
+n=n/10;}
+}
+	return 0;
+}
