9dd91010ac9c7da689a405759a0a7d42fccd7ba3
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-02-08 14:18:44 +0530

    Create sum of num is euen.c

---
+++ sum of num is euen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,product;
+	scanf("%d%d",&a,&b);
+	product=a*b;
+	if(product%2==0)
+{
+printf("the number is even");
+}
+else
+{
+	printf("the number is odd");
+	
+}
+return 0;
+}
