5a820a78dba016b3e37a86b18503e541aff6afde
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-02-27 10:52:31 +0000

    Create PRINT ALPHABET.C

---
+++ PRINT ALPHABET.C
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+ {
+	int n;
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+		printf("A");
+		break;
+	
+		case 2:
+		printf("B");
+		break;
+		case 3:
+		printf("C");
+		break;
+		case 4:
+		printf("D");
+		break;
+		case 5:
+		printf("E");
+		break;
+		case 6:
+		printf("F");
+		break;
+		case 7:
+		printf("G");
+		break;
+		case 8:
+		printf("H");
+		break;
+		case 9:
+		printf("I");
+		break;
+		case 10:
+		printf("J");
+		break;
+	default:
+	break;
+		
+	}
+	return 0;
+}
