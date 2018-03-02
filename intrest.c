690bfb6d3130b12582856969f9797b091d0794c7
Rubakamalakkannan <35257991+Rubakamalakkannan@users.noreply.github.com>
2018-03-02 11:23:47 +0000

    Create intrest.c

---
+++ intrest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int t;
+	float p,r,s;
+	p=100;
+	r=4;
+	t=3;
+	s=(p*r*t)/100;
+	printf("%f sinple intrest\n",s);
+	printf("%f rate\n",r);
+	printf("%f principal\n",p);
+	printf("%d time\n",t);
+	
+}
